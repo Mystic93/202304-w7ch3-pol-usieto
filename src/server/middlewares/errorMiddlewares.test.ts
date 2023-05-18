02f045446cbdf4df2f7a0a9cf1c88d1481eb91c7
Pol <polgil24@hotmail.com>
2023-05-18 21:38:05 +0200

    Add notFounError funtion test

--- src/server/middlewares/errorMiddlewares.test.ts
+++ src/server/middlewares/errorMiddlewares.test.ts
@@ -1,6 +1,6 @@
 import { type NextFunction, type Request, type Response } from "express";
-import type CustomError from "../../CustomError/CustomError.js";
-import { generalError } from "./errorMiddlewares.js";
+import CustomError from "../../CustomError/CustomError.js";
+import { generalError, notFoundError } from "./errorMiddlewares.js";
 
 type CustomResponse = Pick<Response, "status" | "json">;
 
@@ -10,6 +10,18 @@ const res: CustomResponse = {
 };
 const req = {};
 const next = jest.fn();
+
+describe("Given a notFoundError function", () => {
+  describe("When it receives a next function", () => {
+    test("Then it should call it with the custom error with status code 404 and message 'Sorry endpoint not found'", () => {
+      const customError = new CustomError(404, "Sorry endpoint not found");
+
+      notFoundError(req as Request, res as Response, next as NextFunction);
+
+      expect(next).toHaveBeenCalledWith(customError);
+    });
+  });
+});
 
 describe("Given a generalError function", () => {
   describe("When called with an unknown error", () => {
